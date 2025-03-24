263a7543a8cc39df9d2807dfcc5a197f648873bf
Victor <victorgabrieldummersouza@gmail.com>
2025-03-24 17:20:54 -0300

    Segundo commit

--- Aula-01/01.js
+++ Aula-01/01.js
@@ -1,1 +1,7 @@
-console.log("rodrigo faro")
+console.log("rodrigo faro");
+
+
+function bananaDePijama (req, res){
+    console.log("as branch servem para diferenciar um codigo do outro agora eu to na branch nova minha nova feature por exemplo e aque nao tem essa mensagem é a sua primeira branch")
+    // agr to na felipe explicações;
+}
